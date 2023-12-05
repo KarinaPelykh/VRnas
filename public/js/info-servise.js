91495d5514e8294be4db20765ba23ec0afb8b0ed
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2023-12-05 22:07:10 +0100

    ADD: section about-as, an djson file

---
+++ public/js/info-servise.js
@@ -0,0 +1,18 @@
+fetch("../../data/info-servise.json")
+  .then((res) => res.json())
+  .then((data) => {
+    const block = document.querySelector(".info-list");
+    block.style.display = "flex";
+    data.forEach((data) => {
+      const infoItem = document.createElement("li");
+      infoItem.style.marginLeft = "50px";
+
+      infoItem.innerHTML = `<img class="info-images"  src="${data.images}" alt="picture"/>
+        <h2 class="info-title">${data.title}</h2>
+        <p class="description">${data.description}</p>
+      `;
+      block.append(infoItem);
+    });
+  });
+
+console.log(5);
