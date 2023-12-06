ed2ac07ece2faa49bf35469e5c435a828d888be1
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2023-12-06 17:53:47 +0100

    UPDATE:webpach.config

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,12 @@ module.exports = {
   entry: "./public/index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "bundle"),
+    path: path.resolve(__dirname, "public"),
   },
   devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
     port: 3000,
   },
 };
