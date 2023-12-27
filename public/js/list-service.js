6583911059aaf5b7a966720e2dbd7d0b1e253abe
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2023-12-27 21:26:02 +0100

    UPDATE:  detail-service.js

--- public/js/list-service.js
+++ public/js/list-service.js
@@ -7,22 +7,29 @@ fetch("../data/details-service.json")
       const handelClick = (event) => {
         const e = event.currentTarget;
 
-        const previousInfo = document.querySelector("article");
-        console.log(previousInfo);
-        if (previousInfo === true) {
+        const previousInfo = document.querySelector(".cir-div");
+        const classActive = document.querySelector(".active");
+
+        if (previousInfo && classActive) {
           previousInfo.remove();
+          classActive.classList.remove("active");
         }
 
         const divInfo = document.createElement("article");
         divInfo.classList.add("cir-div");
+        element.classList.add("active");
+
         divInfo.innerHTML = `<div class="cir-text">
           <p class="information-text">${data[index].description}</p>
           <p>${data[index].depiction}</p>
           <ul class="list-box">
+
             <p class="decription-list-box">${data[index].list}</p>
           </ul>
           <p class="information-text">${data[index].info}</p>
-          <p>${data[index].inspire}</p>
+          <p>${data[index].commitment}</p>
+
+          <div class="button wrapper-button-contact"> <a class="link " href="./contact_us.html">Contact us</a></div>
         </div>`;
         e.append(divInfo);
 
@@ -30,52 +37,45 @@ fetch("../data/details-service.json")
         data[index].items.forEach((i) => {
           const item = document.createElement("li");
           item.classList.add("item-svg-info");
-          item.innerHTML = `<p>${i}</p>`;
+          item.innerHTML = `${svgChecked} <p class="text-description">${i}</p>`;
           list.append(item);
         });
       };
 
       element.addEventListener("click", handelClick);
     });
+  })
+  .catch((error) => {
+    console.log(error);
   });
-
-// const item = document.querySelectorAll(".item-info-service");
-// fetch("../data/details-service.json")
-//   .then((res) => res.json())
-//   .then((data) => {
-//     data.forEach((element, index) => {
-//       const handelClick = (event) => {
-//         const e = event.currentTarget;
-
-//         // const previousInfo = document.querySelector(".cir-div");
-//         // previousInfo.remove();
-//         // if (previousInfo) {
-//         // }
-
-//         const divInfo = document.createElement("article");
-//         divInfo.classList.add("cir-div");
-//         divInfo.innerHTML = `<div class="cir-text">
-//         <p  class="information-text">${data[index].description}</p>
-//        <p >${element[index].depiction}</p>
-//   <ul class="list-box">
-//   <p class="decription-list-box">${element[index].list}</p>
-//   </ul>
-//    <p class="information-text">${element[index].info}</p>
-//    <p>${element[index].inspire}</p>
-//         </div>`;
-//         e.append(divInfo);
-
-//         const list = document.querySelector(".list-box");
-//         element[index].items.map((i) => {
-//           const item = document.createElement("li");
-//           item.classList.add("item-svg-info");
-//           item.innerHTML = `<p>${i}</p>`;
-//           list.append(item);
-//         });
-//       };
-
-//       item.forEach((item) => {
-//         item.addEventListener("click", handelClick);
-//       });
-//     });
-//   });
+// svg checked
+const svgChecked = `<svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_21_12651)">
+      <path
+        d="M4.35556 6.31111L3.11111 7.55556L7.11111 11.5556L16 2.66667L14.7556 1.42222L7.11111 9.06667L4.35556 6.31111ZM14.2222 14.2222H1.77778V1.77778H10.6667V0H1.77778C0.8 0 0 0.8 0 1.77778V14.2222C0 15.2 0.8 16 1.77778 16H14.2222C15.2 16 16 15.2 16 14.2222V7.11111H14.2222V14.2222Z"
+        fill="url(#paint0_linear_21_12651)"
+      />
+    </g>
+    <defs>
+      <linearGradient
+        id="paint0_linear_21_12651"
+        x1="0"
+        y1="8"
+        x2="16"
+        y2="8"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#0CBAF1" />
+        <stop offset="1" stop-color="#E95CE9" />
+      </linearGradient>
+      <clipPath id="clip0_21_12651">
+        <rect width="16" height="16" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>`;
