91e309f205ff1db51dae6be1cbcfae5cd0ad21d8
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2024-01-10 20:04:43 +0100

    UPDATE:pages add brpo daun list , link on main page

--- public/js/video.js
+++ public/js/video.js
@@ -1,25 +1,25 @@
-import Player from "@vimeo/player";
-const KEY_SECOND_VIDEO = "seconds";
-const video = document.querySelector("iframe");
-const player = new Player(video);
-const onPlay = (data) => {
-  const seconds = data.seconds;
-  localStorage.setItem(KEY_SECOND_VIDEO, seconds);
-};
+// import Player from "@vimeo/player";
+// const KEY_SECOND_VIDEO = "seconds";
+// const video = document.querySelector("iframe");
+// const player = new Player(video);
+// const onPlay = (data) => {
+//   const seconds = data.seconds;
+//   localStorage.setItem(KEY_SECOND_VIDEO, seconds);
+// };
 
-const saveSecond = localStorage.getItem(KEY_SECOND_VIDEO);
+// const saveSecond = localStorage.getItem(KEY_SECOND_VIDEO);
 
-player.on("timeupdate", onPlay);
+// player.on("timeupdate", onPlay);
 
-player
-  .setCurrentTime(saveSecond)
-  .then(function (seconds) {})
-  .catch(function (error) {
-    switch (error.name) {
-      case "RangeError":
-        break;
+// player
+//   .setCurrentTime(saveSecond)
+//   .then(function (seconds) {})
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case "RangeError":
+//         break;
 
-      default:
-        break;
-    }
-  });
+//       default:
+//         break;
+//     }
+//   });
