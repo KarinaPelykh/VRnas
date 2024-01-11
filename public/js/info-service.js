592a9674ff2ee141535692a074c59b8b57a389af
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2024-01-11 23:06:01 +0100

    ADD: adaptation on header,aboutUS,section VirtulReality

--- public/js/info-service.js
+++ public/js/info-service.js
@@ -2,10 +2,9 @@ fetch("../../data/info-service.json")
   .then((res) => res.json())
   .then((data) => {
     const block = document.querySelector(".info-list");
-    block.style.display = "flex";
+
     data.forEach((data) => {
       const infoItem = document.createElement("li");
-      infoItem.style.marginLeft = "50px";
 
       infoItem.innerHTML = `<img class="info-images"  src="${data.images}" alt="picture"/>
         <h2 class="info-title">${data.title}</h2>
