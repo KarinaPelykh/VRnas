ecdde723fbf512645869c8dfa3a9a145a5c0bc3f
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2024-01-23 12:10:02 +0100

    ADD:chande adaptation

--- public/index.js
+++ public/index.js
@@ -1,13 +1,14 @@
 import "./js/info-service";
 import "./js/our-Service";
-import "./js/form-whyChoose";
-import "./js/list-svg";
-import "./js/slider";
-import "./js/news-insights";
-import "./js/footer";
+// import "./js/form-whyChoose";
+// import "./js/list-svg";
+// import "./js/slider";
+// import "./js/news-insights";
+// import "./js/footer";
 import "./js/svgADD/svg-about-us";
-import "./js/svgADD/svg-section-pricing";
-import "./js/modalWindov";
-import "./js/form-subscribe.js";
+// import "./js/svgADD/svg-section-pricing";
+// import "./js/modalWindov";
+// import "./js/form-subscribe.js";
+
+// import "./js/form-whyChoose";
 // import "./js/video.js";
-import "./js/form-whyChoose";
