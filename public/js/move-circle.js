7f54f9c7dde5b8290a1fca49ccc5855ee32470af
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2024-02-01 13:38:08 +0100

    ADD: changes

---
+++ public/js/move-circle.js
@@ -0,0 +1,19 @@
+// const ref = {
+//   buttonLeft: document.querySelector(".js-button-left"),
+//   buttonRight: document.querySelector(".js-button-right"),
+//   items: document.querySelectorAll(".js-item"),
+// };
+// const handelMoveButtonLeft = () => {
+//   ref.items.forEach((item) => {
+//     console.log(item);
+//     item.style.top = "-47";
+//     item.style.left = " 458px;";
+//   });
+// };
+
+// const handelMoveButtonRight = () => {
+//   corner -= 20;
+//   ref.circle.style.transform = `rotate(${corner}deg)`;
+// };
+// ref.buttonLeft.addEventListener("click", handelMoveButtonLeft);
+// ref.buttonRight.addEventListener("click", handelMoveButtonRight);
